ca2d940c1dd8244f0d749ca18c11c2b0a8aad77d
DSosa <sosamorianadav@gmail.com>
2025-04-03 19:13:17 +0100

    Update changes

--- middleware.ts
+++ middleware.ts
@@ -1,3 +1,8 @@
 export { default } from "next-auth/middleware";
 
-export const config = { matcher: ["/"] };
+export const config = {
+  matcher: [
+    // Protege todas las rutas excepto la página de inicio de sesión y otras rutas públicas
+    "/((?!api|_next/static|_next/image|favicon.ico|login|register).*)"
+  ]
+};
